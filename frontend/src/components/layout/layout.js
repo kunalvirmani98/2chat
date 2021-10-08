6d00f1ce074c087cb9c8c10df8c65111eeb4c994
Kunal Virmani <kunalvirmani@Kunals-MacBook-Pro.local>
2021-10-08 22:00:19 +0530

    added login page ui

--- frontend/src/components/layout/layout.js
+++ frontend/src/components/layout/layout.js
@@ -4,41 +4,47 @@ import colorScheme from "../../constants/constant";
 import Login from "../login/login";
 
 class Layout extends React.Component {
-    render() {
-        return (
-            <Login />
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+            userLoggedIn: false,
+            username: ""
+        }
     }
-}
+    handleLogin = (username) => {
+        this.setState({ userLoggedIn: true, username })
+    }
 
-class Layout2 extends React.Component {
     render() {
         const grey = colorScheme.misc;
-        return (
-            <Grid
-                container
-                //direction="row"
-                //justifyContent="center"
-                //alignItems="center"
-                spacing={1}
-                sx={{
-                    background: `${grey}`
-                }}>
-                <Grid item xs={2}>
+        const userLoggedIn = this.state.userLoggedIn;
+        return (<>
+            {!userLoggedIn ? <Login handleLogin={this.handleLogin} /> :
+                <Grid
+                    container
+                    //direction="row"
+                    //justifyContent="center"
+                    //alignItems="center"
+                    spacing={1}
+                    sx={{
+                        background: `${grey}`
+                    }}>
+                    <Grid item xs={2}>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Box
+                            sx={{
+                                margin: "5vh 0",
+                                width: "100%",
+                                height: "90vh",
+                                background: "white",
+                                boxShadow: "2px 2px 5px lightgray"
+                            }}>{this.props.children}</Box>
+                    </Grid>
+                    <Grid item xs={2}>
+                    </Grid>
                 </Grid>
-                <Grid item xs={8}>
-                    <Box
-                        sx={{
-                            margin: "5vh 0",
-                            width: "100%",
-                            height: "90vh",
-                            background: "white",
-                            boxShadow: "2px 2px 5px lightgray"
-                        }}>{this.props.children}</Box>
-                </Grid>
-                <Grid item xs={2}>
-                </Grid>
-            </Grid>
+            }</>
         );
     }
 }
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import colorScheme from '../../constants/constant';
-import { Box, TextField, Typography } from '@mui/material';
+import { Box, Button, Divider, TextField, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const CssLoginField = styled(TextField)({
@@ -11,11 +11,10 @@ const CssLoginField = styled(TextField)({
         borderBottomColor: `${colorScheme.primary}`,
     },
     '& .MuiOutlinedInput-root': {
-        width: "25ch",
+        width: "30ch",
         '& fieldset': {
             borderColor: `${colorScheme.primary}`,
-            borderRadius: "1rem",
-            width: "100%",
+            borderRadius: "0.2rem",
         },
         '&:hover fieldset': {
             borderColor: `${colorScheme.primary}`,
@@ -26,19 +25,27 @@ const CssLoginField = styled(TextField)({
     },
 });
 
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(colorScheme.primary),
+    backgroundColor: colorScheme.primary,
+    opacity: 0.6,
+    '&:hover': {
+        opacity: 1,
+        backgroundColor: colorScheme.primary
+    },
+}));
+
 class Login extends React.Component {
     constructor() {
         super();
         this.state = {
             username: "",
             password: "",
-            isComplete: false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(e) {
-        e.preventDefault();
         this.props.handleLogin(this.state.username);
         // this.setState({ username: e.target})
     }
@@ -70,25 +77,28 @@ class Login extends React.Component {
                 <Box
                     sx={{
                         margin: "5vh 0",
-                        width: "25%",
+                        width: "20%",
                         background: "white",
                         boxShadow: "2px 2px 5px lightgray",
                         display: "flex",
                         flexDirection: "column",
                         justifyContent: "center",
                         alignItems: "center",
-                        padding: "0.5rem"
+                        padding: "1rem",
+                        minHeight: "40%"
                     }}
                     component="form"
                     noValidate
                     autoComplete="off">
                     <Typography variant="h3" gutterBottom component="div">
-                        2Chat
+                        <font color={colorScheme.primary}>2</font>Chat
                     </Typography>
-                    <CssLoginField id="outlined-basic" label="Username" variant="outlined" />
-                    <CssLoginField id="outlined-basic" label="Password" variant="outlined" />
+                    <Divider variant="middle" sx={{ width: "60%", background: `${colorScheme.primary}`, marginBottom: "3rem" }} />
+                    <CssLoginField id="outlined-basic" label="Username" variant="outlined" sx={{ marginBottom: "1rem" }} onChange={(e) => this.handleChange("username", e)} />
+                    <CssLoginField id="outlined-basic" label="Password" variant="outlined" type="password" sx={{ marginBottom: "2rem" }} onChange={(e) => this.handleChange("password", e)} />
+                    <ColorButton variant="contained" onClick={this.handleSubmit.bind(this)}>LOGIN</ColorButton>
                 </Box>
-            </Box>
+            </Box >
 
         )
     }
